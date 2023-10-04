9ec2fb97077eb7b0c986d77fb38ad8eef3491a19
Grelgn <grelgn@hotmail.com>
2023-10-05 00:51:23 +0300

    Add the ability to generate new grids.

--- javascript.js
+++ javascript.js
@@ -17,3 +17,31 @@ for (i = 0; i < 16; i++) {
     container.appendChild(row);
 }
 
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+    let gridSize = 101
+    while (gridSize > 100 || gridSize < 2) {
+        gridSize = prompt("Enter the number of squares per side for the grid:");
+    }
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.setAttribute("class", "row");
+    
+        for (j = 0; j < gridSize; j++) {
+            const square = document.createElement('div');
+            square.setAttribute("class", "square");
+            square.addEventListener('mouseenter', () => {
+                square.classList.add('hover');
+            });
+    
+            row.appendChild(square);
+        }
+    
+        container.appendChild(row);
+    }
+})
